11d13c4d7b11cbeae831f42aa9e38ffd0850212c
alexis <alexis.deroo.ad@gmail.com>
2021-11-25 17:57:51 +0100

    yolo2

--- src/EdgeConProblem/EdgeConResolution.c
+++ src/EdgeConProblem/EdgeConResolution.c
@@ -191,6 +191,7 @@ unsigned int getMinCost(EdgeConGraph graph, int source, int target) {
                     if (counter > k) {
                         color[source] = 1;
                         source = pop(s);
+                        printf("source = %d\n", source);
                         int * nTmp = (int *) malloc(sizeof(int)*graphOrder);
                         getNeighborhood(getGraph(graph), source, nTmp);
                         setGreyNeighborhoodInWhith(neighborhood, nTmp, color);
@@ -207,6 +208,7 @@ unsigned int getMinCost(EdgeConGraph graph, int source, int target) {
             }
         }
 
+        printf("source = %d\n", source);
         neighborhoodNumber = getNeighborhood(getGraph(graph), source, neighborhood);
         if (source != -1) {
             push(s, source);
